cb2be192a547b50f5ad7672c6143cae56d544330
asackan <addwood1@naver.com>
2023-05-22 13:23:47 +0900

    example 2 추가

--- example2_bank/accounts/accounts.go
+++ example2_bank/accounts/accounts.go
@@ -1,11 +1,59 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Account struct {
 	owner   string
 	balance int
 }
 
-func (a Account) NewAccount(owner string) *Account {
+var errNoMoney = errors.New("* Can't withdraw. You don't have that much\n")
+
+// NewAccount creats Account
+func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	fmt.Println("Try Deposit", amount)
+	a.balance += amount
+	fmt.Println("* clear\n")
+}
+
+// return Balance of your Account
+func (a *Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x ammount from your Account
+func (a *Account) Withdraw(amount int) error {
+	fmt.Println("Try Withdraw", amount)
+
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	fmt.Println("* clear\n")
+	return nil
+}
+
+// Change Owner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	fmt.Print("Change Account Owner : ", a.owner, " to ")
+	a.owner = newOwner
+	fmt.Println(a.owner, "\n")
+}
+
+// return Owner name
+func (a *Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account. Has:", a.Balance())
 }
--- example2_bank/main.go
+++ example2_bank/main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/asackan/study-golang/example2_bank/accounts"
 )
 
 func main() {
-	account := accounts.NewAccount("nico")
+	account := accounts.NewAccount("juwon")
+
+	account.Deposit(10)
+	fmt.Println(account.Owner(), "have", account.Balance(), "\n")
+
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	account.ChangeOwner("mom")
 	fmt.Println(account)
 }
