b1818b99c6f051ecb4f4fcc0e9fe44bfb3d736c0
asackan <addwood1@naver.com>
2023-05-22 15:28:04 +0900

    예제4 업데이트

---
+++ example4_interface/main.go
@@ -0,0 +1,11 @@
+package main
+
+func SendBook(name string, sender *fedex.FedexSender) {
+	sender.Send(name)
+}
+
+func main() {
+	sender := &fedex.FedexSender{}
+	SendBook("어린 왕자", sender)
+	SendBook("그리스인 조르바", sender)
+}
